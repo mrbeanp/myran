f4bc522dec66aae1362aecf4ce32091ed0c2445c
yadhukrishanyadhukrishan <90526355+yadhukrishanyadhukrishan@users.noreply.github.com>
2022-03-27 20:29:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -179,7 +179,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('𝚃𝙷𝙸𝚂 𝙼𝙾𝚅𝙸𝙴 I𝚂 𝙽𝙾𝚃 𝚈𝙴𝚃 𝚁𝙴𝙻𝙴𝙰𝚂𝙴𝙳 𝙾𝚁 𝙰𝙳𝙳𝙴𝙳 𝚃𝙾 𝙳𝙰𝚃𝚂𝙱𝙰𝚂𝙴 💌 <b>[𝐔𝐏𝐃𝐀𝐓𝐄𝐃](https://t.me/mallmovieup)')
+            k = await query.message.edit('𝚃𝙷𝙸𝚂 𝙼𝙾𝚅𝙸𝙴 I𝚂 𝙽𝙾𝚃 𝚈𝙴𝚃 𝚁𝙴𝙻𝙴𝙰𝚂𝙴𝙳 𝙾𝚁 𝙰𝙳𝙳𝙴𝙳 𝚃𝙾 𝙳𝙰𝚃𝚂𝙱𝙰𝚂𝙴 💌 <b>[𝚂𝙴𝙰𝚁𝙲𝙷 𝙶𝙾𝙾𝙶𝙻𝙴](https://www.google.com/search?q=)')
             await asyncio.sleep(10)
             await k.delete()
 
